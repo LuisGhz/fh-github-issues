4775dcf144acfb516ec29b99110cdea11ab0069d
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-10-02 15:49:02 -0600

    Create issue page

---
+++ src/app/src/app/modules/issues/pages/issue/issue-page.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { IssuePage } from './issue-page';
+
+describe('IssuePage', () => {
+  let component: IssuePage;
+  let fixture: ComponentFixture<IssuePage>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [IssuePage]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(IssuePage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/src/app/modules/issues/pages/issue/issue-page.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'issue-page',
+  imports: [],
+  templateUrl: './issue-page.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class IssuePage {
+
+}
