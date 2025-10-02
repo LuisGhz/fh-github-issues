ea451ef4fdc8f1863ab9a6fdf6af7ae351f6afb9
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-10-02 16:10:05 -0600

    Add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'issues',
+    loadComponent: () =>
+      import('./modules/issues/pages/issues-list/issues-list-page').then((m) => m.IssuesListPage),
+  },
+  {
+    path: 'issues/:id',
+    loadComponent: () => import('./modules/issues/pages/issue/issue-page').then((m) => m.IssuePage),
+  },
+  {
+    path: '**',
+    redirectTo: 'issues',
+  },
+];
