d4a169def0027f68510b076692c99bb9d2e91c87
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-10-03 11:06:02 -0600

    Add labels selector

--- src/app/modules/issues/pages/issues-list/issues-list-page.ts
+++ src/app/modules/issues/pages/issues-list/issues-list-page.ts
@@ -1,10 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { IssuesService } from '../../services/issues.service';
+import { LabelsSelector } from '../../components/labels-selector';
 
 @Component({
   selector: 'issues-list-page',
-  imports: [RouterLink],
+  imports: [RouterLink, LabelsSelector],
   templateUrl: './issues-list-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class IssuesListPage {}
+export class IssuesListPage {
+  issuesService = inject(IssuesService);
+
+  get labelsQuery() {
+    return this.issuesService.labelsQuery;
+  }
+}
