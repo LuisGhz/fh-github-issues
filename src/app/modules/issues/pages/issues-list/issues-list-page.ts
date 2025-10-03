2f6d662d69838ceb03ea84fac576f74198928ce7
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-10-03 15:15:45 -0600

    Implement state selection

--- src/app/modules/issues/pages/issues-list/issues-list-page.ts
+++ src/app/modules/issues/pages/issues-list/issues-list-page.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { IssuesService } from '../../services/issues.service';
 import { LabelsSelector } from '../../components/labels-selector';
 import { IssueItem } from '../../components/issue-item';
+import { State } from '../../interfaces/github-issue.interface';
 
 @Component({
   selector: 'issues-list-page',
@@ -19,5 +20,15 @@ export class IssuesListPage {
 
   get issuesQuery() {
     return this.issuesService.issuesQuery;
+  }
+
+  get selectedState() {
+    return this.issuesService.selectedState();
+  }
+
+  showIssuesByState(newState: 'open' | 'closed' | 'all') {
+    const state =
+      newState === 'open' ? State.Open : newState === 'closed' ? State.Closed : State.All;
+    this.issuesService.showIssuesByState(state);
   }
 }
