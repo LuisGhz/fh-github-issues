1783d2635288ad67d94105a5986f7ddc907f0dd6
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-10-03 10:36:42 -0600

    Create labels selector

---
+++ src/app/modules/issues/components/labels-selector.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'labels-selector',
+  imports: [],
+  templateUrl: './labels-selector.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LabelsSelector {
+
+}
