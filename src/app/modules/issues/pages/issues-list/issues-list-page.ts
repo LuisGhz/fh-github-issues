571ea808fb10b487f55870a85a99fc2edb2d82ea
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-10-03 11:42:32 -0600

    Add issue item

--- src/app/modules/issues/pages/issues-list/issues-list-page.ts
+++ src/app/modules/issues/pages/issues-list/issues-list-page.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IssuesService } from '../../services/issues.service';
 import { LabelsSelector } from '../../components/labels-selector';
+import { IssueItem } from '../../components/issue-item';
 
 @Component({
   selector: 'issues-list-page',
-  imports: [CommonModule, LabelsSelector],
+  imports: [CommonModule, LabelsSelector, IssueItem],
   templateUrl: './issues-list-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
