a143e83402c7290974277603bb430059be2b97b5
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-10-03 11:56:53 -0600

    Create issue comment

---
+++ src/app/modules/issues/components/issue-comment.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'issue-comment',
+  imports: [],
+  templateUrl: './issue-comment.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class IssueComment {
+
+}
