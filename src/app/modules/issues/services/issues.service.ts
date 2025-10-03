4f59da8f3f4427c292ab13d2abac79cf3ce7c339
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-10-03 15:15:11 -0600

    Add issue state

--- src/app/modules/issues/services/issues.service.ts
+++ src/app/modules/issues/services/issues.service.ts
@@ -1,19 +1,31 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 import { injectQuery } from '@tanstack/angular-query-experimental';
 import { getLabels } from '../actions/get-labels.action';
 import { getIssues } from '../actions/get-issues.action';
+import { State } from '../interfaces/github-issue.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class IssuesService {
+  #selectedState = signal<State>(State.All);
+
   labelsQuery = injectQuery(() => ({
     queryKey: ['labels'],
     queryFn: () => getLabels(),
   }));
 
   issuesQuery = injectQuery(() => ({
-    queryKey: ['issues'],
-    queryFn: () => getIssues(),
+    queryKey: ['issues', this.#selectedState()],
+    queryFn: () => getIssues(this.#selectedState()),
   }));
+
+  selectedState() {
+    return this.#selectedState.asReadonly();
+  }
+
+  showIssuesByState(state: State) {
+    console.log(state);
+    this.#selectedState.set(state);
+  }
 }
