acd1c10c716af725522ecd881af01ddd07e61656
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-10-03 11:28:49 -0600

    Add environment variables

--- src/app/modules/issues/actions/get-labels.ts
+++ src/app/modules/issues/actions/get-labels.ts
@@ -1,10 +1,15 @@
 import { sleep } from '@helpers/sleep';
 import { GithubLabel } from '../interfaces';
+import { environment } from 'src/environments/environment.development';
 
 export const getLabels = async () => {
   await sleep(3000);
   try {
-    const response = await fetch('https://api.github.com/repos/angular/angular/labels');
+    const response = await fetch(`${environment.baseUrl}/labels`, {
+      headers: {
+        Authorization: `Bearer ${environment.githubToken}`,
+      },
+    });
     if (!response.ok) throw new Error('Failed to fetch labels');
     const data = (await response.json()) as GithubLabel[];
     return data;
