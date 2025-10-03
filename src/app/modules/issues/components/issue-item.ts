2e8e2a22e8ca87fd44f081b0a71d4e209a0fac3b
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-10-03 11:42:23 -0600

    Create layout

--- src/app/modules/issues/components/issue-item.ts
+++ src/app/modules/issues/components/issue-item.ts
@@ -1,11 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { GithubIssue } from '../interfaces';
 
 @Component({
   selector: 'issue-item',
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './issue-item.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class IssueItem {
-
+  issue = input.required<GithubIssue>();
+  isOpen = computed(() => this.issue().state === 'open');
 }
