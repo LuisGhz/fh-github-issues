839bd27eae135326a11fc9ea232dc5cb0fccc9e2
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-10-03 11:05:50 -0600

    Show labels

--- src/app/modules/issues/components/labels-selector.ts
+++ src/app/modules/issues/components/labels-selector.ts
@@ -1,11 +1,13 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { GithubLabel } from '../interfaces';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'labels-selector',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './labels-selector.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LabelsSelector {
-
+  labels = input.required<GithubLabel[]>();
 }
