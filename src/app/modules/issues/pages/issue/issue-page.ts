fe8354a5910040685873bc65df31378598bf6ede
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-10-03 12:05:38 -0600

    Get issue by number and show it once loaded

--- src/app/modules/issues/pages/issue/issue-page.ts
+++ src/app/modules/issues/pages/issue/issue-page.ts
@@ -1,21 +1,26 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { map, tap } from 'rxjs';
+import { IssueService } from '../../services/issue.service';
+import { IssueComment } from '../../components/issue-comment';
 
 @Component({
   selector: 'issue-page',
-  imports: [],
+  imports: [RouterLink, IssueComment],
   templateUrl: './issue-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class IssuePage {
   #activatedRoute = inject(ActivatedRoute);
+  #issueService = inject(IssueService);
 
   issueNumber = toSignal(
     this.#activatedRoute.paramMap.pipe(
       map((params) => Number(params.get('number') ?? 0)),
-      tap(console.log),
+      tap((n) => this.#issueService.setIssueNumber(n)),
     ),
   );
+
+  issueQuery = this.#issueService.issuesQuery;
 }
