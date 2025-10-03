fb2945036b55e149eadd5c5caea48427149f6bc0
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-10-03 11:57:11 -0600

    Create issue service

---
+++ src/app/modules/issues/services/issue.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, signal } from '@angular/core';
+import { injectQuery } from '@tanstack/angular-query-experimental';
+import { getIssueByNumber } from '../actions/get-issue-by-number.action';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class IssueService {
+  #issueNumber = signal<number | null>(null);
+
+  issuesQuery = injectQuery(() => ({
+    queryKey: ['issue', this.#issueNumber()],
+    queryFn: () => getIssueByNumber(this.#issueNumber()!),
+    enabled: this.#issueNumber() !== null,
+  }));
+
+  setIssueNumber(number: number) {
+    this.#issueNumber.set(number);
+  }
+}
