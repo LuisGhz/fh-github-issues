701b11fd2b1e623ab78b29eb2a2caf3e2c8df147
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-10-03 12:01:45 -0600

    Show issue comment

--- src/app/modules/issues/components/issue-comment.ts
+++ src/app/modules/issues/components/issue-comment.ts
@@ -1,11 +1,12 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { GithubIssue } from '../interfaces';
 
 @Component({
   selector: 'issue-comment',
   imports: [],
   templateUrl: './issue-comment.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class IssueComment {
-
+  issue = input.required<GithubIssue>();
 }
