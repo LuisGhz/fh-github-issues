d3c21092ddef0016fe9c5dc3384b014718f53a2e
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-10-03 11:37:29 -0600

    Create issue item

---
+++ src/app/modules/issues/components/issue-item.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'issue-item',
+  imports: [],
+  templateUrl: './issue-item.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class IssueItem {
+
+}
