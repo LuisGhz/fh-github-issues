96a4bc6bd2e00059aa4b43ab6f5f2299df2a9652
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-10-03 11:46:08 -0600

    Get issue number from route params

--- src/app/modules/issues/pages/issue/issue-page.ts
+++ src/app/modules/issues/pages/issue/issue-page.ts
@@ -1,11 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { map, tap } from 'rxjs';
 
 @Component({
   selector: 'issue-page',
   imports: [],
   templateUrl: './issue-page.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class IssuePage {
+  #activatedRoute = inject(ActivatedRoute);
 
+  issueNumber = toSignal(
+    this.#activatedRoute.paramMap.pipe(
+      map((params) => Number(params.get('number') ?? 0)),
+      tap(console.log),
+    ),
+  );
 }
